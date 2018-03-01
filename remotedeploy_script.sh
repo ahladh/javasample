23d264ce769a61e085ca554b8233866386e4843f
ahladh <ahladhreddyc@gmail.com>
2018-03-01 11:36:56 +0530

    push script

---
+++ remotedeploy_script.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+sh ahladh@54.244.209.186 '
+var=$(ps -ef | grep -v grep | grep tomcat8 | wc -l )
+
+if [ $var -lt 1 ]
+   then
+  	sudo service tomcat8 start
+fi
+
+
+
+cd ./SpringMVCSecurityXML/target
+mv SpringMVCSecurityXML.war 1213_by_script.war
+scp 1213_by_script.war ahladh@54.244.209.186:/var/lib/tomcat8/webapps
